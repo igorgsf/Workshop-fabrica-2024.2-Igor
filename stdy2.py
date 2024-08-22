b76ddbb2d9f53468f7bfc9392a7780325f1215e0
Igor Gabriel <132006267+igorgsf@users.noreply.github.com>
2024-08-22 07:40:25 -0300

    Add files via upload
    Praticando

---
+++ stdy2.py
@@ -0,0 +1,11 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+n3 = int(input("Digite o terceiro número: "))
+
+#Dito os valores dos números, como calculo a média
+
+media = (n1 + n2 + n3) / 3
+
+#Agora, PRINTE
+
+print("A média dos 3 numeros é: " + str(media))
