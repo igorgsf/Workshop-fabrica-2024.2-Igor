3b788fc160729c42d767d75818415e77ad1aa7d7
Igor Gabriel <132006267+igorgsf@users.noreply.github.com>
2024-08-22 11:00:32 -0300

    Add files via upload
    Nesse código, aprendi como usar STRIP E LOWER.

---
+++ e5.py
@@ -0,0 +1,23 @@
+# EX5 - DESCONTO EM COMPRA
+
+while True:
+
+    valor = float(input("Qual valor da compra? "))
+
+    if valor >= 500:
+        print("Você ganhou 20% de desconto. O valor da sua compra com o desconto é: R$ " + str(valor*0.8))
+    elif valor >= 250:
+        print("Você ganou 10% de desconto. O valor da sua compra com desconto é: R$ " + str(valor*0.9))
+    else:
+        print("Você não ganou nenhum desconto. O valor da sua compra é: R$ "+ str(valor))
+
+    # Perguntar ao usuário se ele gostaria de calcular outro valor
+    
+    resposta = input("Você deseja calcular outro valor? (sim/não) ").strip().lower() 
+    #Strip remove os espaços em branco extras no início e no final da string.
+    #lower Converte todos os caracteres da string para minúsculas.
+
+    if resposta == 'não':
+        break
+
+    print("Ok, vamos continuar!")
